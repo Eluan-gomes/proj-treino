35413f010f5513d80a7b2523e9a412dc2ac30735
Vkop-star <eluangomesalencar@gmail.com>
2023-11-20 13:37:10 -0300

    atu

--- js/index.js
+++ js/index.js
@@ -1,22 +1,18 @@
-
 function sortear() {
+    var numeroInicial = parseInt(document.getElementById("numero_inicial").value);
+    var numeroFinal = parseInt(document.getElementById("numero_final").value);
 
-    var numeroInicial = document.getElementById("numero_inicial").value;
-
-    var numeroFinal = document.getElementById("numero_final").value;
-
-
-    if (numeroInicial > numeroFinal) {
-
-        alert("O número inicial não pode ser maior que o número final.");
-
+    if (isNaN(numeroInicial) || isNaN(numeroFinal)) {
+        alert("Por favor, insira números válidos.");
         return;
-
     }
 
+    if (numeroInicial > numeroFinal) {
+        alert("O número inicial não pode ser maior que o número final.");
+        return;
+    }
 
     var numeroSorteado = Math.floor(Math.random() * (numeroFinal - numeroInicial + 1)) + numeroInicial;
 
     document.getElementById("resultado").innerHTML = "Número sorteado: " + numeroSorteado;
-
 }
