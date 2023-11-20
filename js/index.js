d7b12f893921c9de964ece23717b04148bdbb5d4
Vkop-star <eluangomesalencar@gmail.com>
2023-11-20 13:49:11 -0300

    atu

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 function sortear() {
     var numeroInicial = parseInt(document.getElementById("numero_inicial").value);
     var numeroFinal = parseInt(document.getElementById("numero_final").value);
+    var quantidadeResultados = parseInt(document.getElementById("quantidade_resultados").value);
 
-    if (isNaN(numeroInicial) || isNaN(numeroFinal)) {
+    if (isNaN(numeroInicial) || isNaN(numeroFinal) || isNaN(quantidadeResultados)) {
         alert("Por favor, insira números válidos.");
         return;
     }
@@ -12,7 +13,29 @@ function sortear() {
         return;
     }
 
-    var numeroSorteado = Math.floor(Math.random() * (numeroFinal - numeroInicial + 1)) + numeroInicial;
+    var resultados = [];
 
-    document.getElementById("resultado").innerHTML = "Número sorteado: " + numeroSorteado;
+    for (var i = 0; i < quantidadeResultados; i++) {
+        var numeroSorteado = Math.floor(Math.random() * (numeroFinal - numeroInicial + 1)) + numeroInicial;
+        resultados.push(numeroSorteado);
+    }
+
+    var resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = "Números sorteados: " + resultados.join(", ");
+
+    // Criar PDF e adicionar resultados
+    var pdf = new jsPDF();
+    pdf.text("Números Sorteados", 10, 10);
+    for (var i = 0; i < resultados.length; i++) {
+        pdf.text("Resultado " + (i + 1) + ": " + resultados[i], 10, 20 + i * 10);
+    }
+
+    // Salvar PDF
+    var pdfBlob = pdf.output("blob");
+    var pdfUrl = URL.createObjectURL(pdfBlob);
+
+    var downloadLink = document.createElement("a");
+    downloadLink.href = pdfUrl;
+    downloadLink.download = "resultados.pdf";
+    downloadLink.click();
 }
